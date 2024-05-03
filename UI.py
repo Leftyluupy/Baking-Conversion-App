ba3905a92399c9f20145e063a3b38c7493b294f4
Leftyluupy <99181178+Leftyluupy@users.noreply.github.com>
2024-05-03 15:45:55 +0900

    first commit

---
+++ UI.py
@@ -0,0 +1,6 @@
+# Code by Luann Pascucci
+# initially starting out by following https://tkdocs.com/tutorial/intro.html
+# and then modifying as needed
+
+# This is part of a project for CS361 at Oregon State University.
+
